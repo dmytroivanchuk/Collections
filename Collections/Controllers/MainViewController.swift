492d37094931ea472bfa130a2a857bf57d717223
Dmytro Ivanchuk <dmytroivanchuk1998@gmail.com>
2022-08-10 22:22:11 +0300

    set up initial menuTableView

--- Collections/Controllers/MainViewController.swift
+++ Collections/Controllers/MainViewController.swift
@@ -9,11 +9,41 @@ import UIKit
 
 class MainViewController: UIViewController {
 
+    @IBOutlet var menuTableView: UITableView!
+    
+    let cellTitleArray = ["Array", "Set", "Dictionary"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        menuTableView.delegate = self
+        menuTableView.dataSource = self
     }
-
-
 }
 
+//MARK: - UITableViewDelegate Methods
+
+extension MainViewController: UITableViewDelegate {
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+    }
+}
+
+//MARK: - UITableViewDataSource Methods
+
+extension MainViewController: UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        3
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "menuCell", for: indexPath)
+        
+        var content = cell.defaultContentConfiguration()
+        content.text = cellTitleArray[indexPath.row]
+        
+        cell.accessoryType = .disclosureIndicator
+        cell.contentConfiguration = content
+        return cell
+    }
+}
