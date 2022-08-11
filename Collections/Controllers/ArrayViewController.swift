0c205abb2cf670a97811b94452946c49057937c6
Dmytro Ivanchuk <dmytroivanchuk1998@gmail.com>
2022-08-11 12:36:39 +0300

    set up initial ArrayViewController

---
+++ Collections/Controllers/ArrayViewController.swift
@@ -0,0 +1,15 @@
+//
+//  ArrayViewController.swift
+//  Collections
+//
+//  Created by Dmytro Ivanchuk on 11.08.2022.
+//
+
+import UIKit
+
+class ArrayViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
