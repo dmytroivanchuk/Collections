46a0afaecf6d4886606727c444dea54fcfe9f112
Dmytro Ivanchuk <dmytroivanchuk1998@gmail.com>
2022-08-12 16:33:19 +0300

    configure OperationCollectionViewCell, add custom colors to Assets

--- Collections/Views/OperationCollectionViewCell.swift
+++ Collections/Views/OperationCollectionViewCell.swift
@@ -9,10 +9,19 @@ import UIKit
 
 class OperationCollectionViewCell: UICollectionViewCell {
 
+    @IBOutlet var operationView: UIView!
     @IBOutlet var operationDescriptionLabel: UILabel!
     @IBOutlet var operationInProcessActivityIndicatorView: UIActivityIndicatorView!
     
     override func awakeFromNib() {
         super.awakeFromNib()
+        operationView.backgroundColor = UIColor(named: "customDarkGray")
+        
+        operationDescriptionLabel.text = ""
+        operationDescriptionLabel.font = .systemFont(ofSize: 16)
+        operationDescriptionLabel.textColor = .systemBlue
+        operationDescriptionLabel.numberOfLines = 0
+        
+        operationInProcessActivityIndicatorView.hidesWhenStopped = true
     }
 }
