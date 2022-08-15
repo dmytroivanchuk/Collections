30e0c419f0bf8aeabe69d589c4b44652450cc0de
Dmytro Ivanchuk <dmytroivanchuk1998@gmail.com>
2022-08-15 21:15:23 +0300

    create and set up initial SetViewController

---
+++ Collections/Controllers/SetViewController.swift
@@ -0,0 +1,16 @@
+//
+//  SetViewController.swift
+//  Collections
+//
+//  Created by Dmytro Ivanchuk on 15.08.2022.
+//
+
+import UIKit
+
+class SetViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+}
